17aef027335ffcafae07fb8f84b5486e631a2710
DoanCongQui <congt4644@gmail.com>
2023-03-08 14:05:06 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main ()
-{
-    int n;
-    cout <<"n = ";
-    cin >> n;
-    cout << n + n ;
-    return 0;
+int main() {
+  int n;
+  cout << "n = ";
+  cin >> n;
+  cout << n + n;
+  return 0;
 }
